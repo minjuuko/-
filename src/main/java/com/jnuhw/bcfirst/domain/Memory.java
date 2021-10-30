a096fe54baeffc8842450d0d907932117675d744
KGW20278 <retryer_@naver.com>
2021-10-30 12:04:22 +0900

    fix: 메모리가 deepCopy 되지 않는 부분 수정

--- src/main/java/com/jnuhw/bcfirst/domain/Memory.java
+++ src/main/java/com/jnuhw/bcfirst/domain/Memory.java
@@ -7,7 +7,16 @@ import java.util.List;
 public class Memory {
 
     // 4096 개의 16비트 숫자 데이터들
-    private List<BitData> memory = new ArrayList<>(Collections.nCopies(4096, new BitData(16, true)));
+    // private List<BitData> memory = new ArrayList<>(Collections.nCopies(4096, new BitData(16, true)));
+
+    private List<BitData> memory;
+
+    public Memory() {
+        memory = new ArrayList<>();
+        for(int i = 0 ; i < 4096 ; i++) {
+            memory.add(new BitData(16));
+        }
+    }
 
     public void setMemoryData(int address, int data) {
         memory.get(address).setData(data);
