498f97bb405cb0ea55a95d3a7f0dd9ea8f59d4ac
KGW20278 <retryer_@naver.com>
2021-11-10 15:07:40 +0900

    feat: Subroutine을 한줄로 받는 명령어 'SUB' 추가  args >> SH4, OR, MVE

--- src/main/java/com/jnuhw/bcfirst/view/InputView.java
+++ src/main/java/com/jnuhw/bcfirst/view/InputView.java
@@ -3,6 +3,7 @@ package com.jnuhw.bcfirst.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class InputView {
@@ -13,11 +14,67 @@ public class InputView {
         List<String> program = new ArrayList<>();
         String command;
         do {
-            String code = scanner.nextLine();
-            program.add(code);
+            String code = scanner.nextLine().toUpperCase(Locale.ROOT);
+            if(code.startsWith("SUB")) {
+                program = addSubroutine(program, code.split(" ")[1]);
+            } else {
+                program.add(code);
+            }
             command = code.split(" ")[0];
         } while (!command.equals("END"));
 
+        return program;
+    }
+
+    private static List<String> addSubroutine(List<String> program, String key) {
+        switch(key) {
+            case "SH4":
+                program.add("SH4, HEX 0");
+                program.add("CIL");
+                program.add("CIL");
+                program.add("CIL");
+                program.add("CIL");
+                program.add("AND MSK");
+                program.add("BUN SH4 I");
+                program.add("MSK, HEX FFF0");
+                return program;
+            case "OR":
+                program.add("OR, HEX 0");
+                program.add("CMA");
+                program.add("STA TMP");
+                program.add("LDA OR I");
+                program.add("CMA");
+                program.add("AND TMP");
+                program.add("CMA");
+                program.add("ISZ OR");
+                program.add("BUN OR I");
+                program.add("TMP, HEX 0");
+                return program;
+            case "MVE":
+                program.add("MVE, HEX 0");
+                program.add("LDA MVE I");
+                program.add("STA PT1");
+                program.add("ISZ MVE");
+                program.add("LDA MVE I");
+                program.add("STA PT2");
+                program.add("ISZ MVE");
+                program.add("LDA MVE I");
+                program.add("STA CT R");
+                program.add("ISZ MVE");
+                program.add("LOP, LDA PT1 I");
+                program.add("STA PT2 I");
+                program.add("ISZ PT1");
+                program.add("ISZ PT2");
+                program.add("ISZ CTR");
+                program.add("BUN LOP");
+                program.add("BUN MVE I");
+                program.add("PT1, DEC 0");
+                program.add("PT2, DEC 0");
+                program.add("CTR, DEC 0");
+                return program;
+
+
+        }
         return program;
     }
 }
